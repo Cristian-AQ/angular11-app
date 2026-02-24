3b84bedfbc08eda3444f1ddebd8d62df3527af4a
Cristian-Aquino <yojar.aquino@unmsm.edu.pe>
2026-02-23 19:59:55 -0500

    feat: app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PadreComponentComponent } from './padre-component/padre-component.component';
+import { HijoComponentComponent } from './hijo-component/hijo-component.component';
+import { TestModule } from './test/test.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PadreComponentComponent,
+    HijoComponentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    TestModule
   ],
   providers: [],
   bootstrap: [AppComponent]
